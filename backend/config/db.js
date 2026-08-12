705cbab65e68d8887ba45ab88f879b8e2eec5d8a
Maheswari Devireddy <maheswari@example.com>
2026-08-12 23:15:13 +0530

    Secure MongoDB configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb+srv://maheswarid29_db_user:5422173@cluster0.no75xcx.mongodb.net/edutrack?appName=Cluster0");
-        console.log("MongoDB Connected");
-    } catch (error) {
-        console.log(error.message);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 module.exports = connectDB;
