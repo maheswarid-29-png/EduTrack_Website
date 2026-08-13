3cb46e19a4be31eb9f9139801fb7d0b970b4826b
Maheswari Devireddy <maheswari@example.com>
2026-08-13 14:50:56 +0530

    Remove temporary database debug log

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI);
 
         console.log("MongoDB Connected");
-        console.log("Database Name:", mongoose.connection.name);
+        
 
     } catch (error) {
         console.log(error.message);
